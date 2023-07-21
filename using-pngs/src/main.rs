ee032c0c1a18fb2e5d28ee092ea1a9a16b8b7576
Christian Henkel <post@henkelchristian.de>
2023-07-21 20:57:35 +0200

    apples
    Signed-off-by: Christian Henkel <post@henkelchristian.de>

--- using-pngs/src/main.rs
+++ using-pngs/src/main.rs
@@ -1,27 +1,38 @@
-use convolve2d::DynamicMatrix;
-use std::convert::TryInto;
+use image::Pixel;
 use std::env;
-use std::fs::File;
+use image::ImageBuffer;
+use image::Rgb;
 
-fn to_array<T, const N: usize>(v: Vec<T>) -> [T; N] {
-    v.try_into()
-        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", N, v.len()))
-}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert!(args.len() == 2);
     let fname: &String = &args[1];
     println!("fname: {}", fname);
 
-    // Read png into vector
-    let decoder = png::Decoder::new(File::open(fname).unwrap());
-    let mut reader = decoder.read_info().unwrap();
-    let (width, height) = reader.info().size();
-    println!("width: {}, height: {}", width, height);
-    let mut buf = vec![0; reader.output_buffer_size()];
-    let info = reader.next_frame(&mut buf).unwrap();
-    println!("info: {:?}", info);
-    // println!("buf: {:?}", buf);
-    let img = DynamicMatrix::new(width as usize, height as usize, buf);
+    // Read png
+    let source_image: ImageBuffer<Rgb<u8>, Vec<u8>> = image::open(fname)
+        .expect("failed to read image")
+        .to_rgb8();
+    let width = source_image.width();
+    let height = source_image.height();
+
+    // Increase contrast
+    let mut contrast_image: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(width, height);
+    for x in 0..width {
+        for y in 0..height {
+            let pixel = source_image.get_pixel(x, y);
+            let mut pixel_out = [0, 0, 0];
+            for i in 0..3 {
+                let mut val = pixel[i] as f32 / 255.0;
+                val = - (val - 1.0).powf(2.0) + 1.0;
+                pixel_out[i] = (val * 255.0) as u8;
+            }
+            contrast_image.put_pixel(x, y, Rgb(pixel_out));
+        }
+    }
+
+    // Write contrast image
+    contrast_image.save("contrast.png").unwrap();
 
 }
